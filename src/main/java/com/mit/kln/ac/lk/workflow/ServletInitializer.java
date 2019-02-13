73e82ad269657218e04ba271787650175cf62e96
pasan.peiris01 <pasan.peiris01@gmail.com>
2019-02-13 21:26:40 +0530

    initial repo commit

---
+++ src/main/java/com/mit/kln/ac/lk/workflow/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.mit.kln.ac.lk.workflow;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WorkflowApplication.class);
+    }
+
+}
+
