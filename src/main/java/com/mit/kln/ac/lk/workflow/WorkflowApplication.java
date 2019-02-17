1ff7aca2a06db88cd0768e11595ffc825bd05480
Benuka <benuka02@gmail.com>
2019-02-18 00:44:10 +0530

    UserApi Basic

--- src/main/java/com/mit/kln/ac/lk/workflow/WorkflowApplication.java
+++ src/main/java/com/mit/kln/ac/lk/workflow/WorkflowApplication.java
@@ -2,8 +2,11 @@ package com.mit.kln.ac.lk.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableJpaAuditing
 public class WorkflowApplication {
 
     public static void main(String[] args) {
